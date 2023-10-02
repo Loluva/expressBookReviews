dc796db726e993b51e384959668dc752b07d6905
Loluva <alvaromunive99@hotmail.com>
2023-10-02 06:58:42 -0400

    final project completed

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,23 +7,76 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+let userswithsamename=users.filter(user=>user.username==username)
+if(userswithsamename.length>0){
+  return false
+}else{
+    return true
+}
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+console.log(users)    
+let authenticatedUser=users.filter(user=>{
+        return (user.username == username && user.password == password)
+    })
+    if(authenticatedUser.length>0){
+        return true
+    }else{
+        return false
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let username=req.query.username
+  let password=req.query.password
+
+  if(!(username&&password))
+  return res.status(404).send("username or password missing")
+
+  if(authenticatedUser(username,password)){
+    let accessToken = jwt.sign({
+        data: password
+      }, 'access', { expiresIn: 60 * 60 });
+    req.session.authorization = {
+        accessToken,username
+    }
+    return res.status(200).send("User successfully logged in");
+  }else{
+  return res.status(403).send("User is not resgistered")
+  }
+
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let username=req.session.authorization.username
+  let isbn=req.params.isbn
+  let review=req.query.review
+
+  let book=books[isbn]
+  let reviews=book.reviews
+  reviews[username]=review
+  books[isbn].reviews=reviews
+
+  return res.status(200).send(JSON.stringify(books[isbn],null,4));
 });
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    //Write your code here
+    let username=req.session.authorization.username
+    let isbn=req.params.isbn
+  
+    let book=books[isbn]
+    let reviews=book.reviews
+    delete reviews[username]
+    books[isbn].reviews=reviews
+  
+    return res.status(200).send(JSON.stringify(books[isbn],null,4));
+  });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios=require('axios')
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -7,37 +8,97 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let username=req.body.username
+  let password=req.body.password
+  if(!(username&&password))
+  return res.status(404).send("username or password missing")
+
+  if(!isValid(username))
+  return res.status(403).send("username is alrady used")
+
+  users.push({"username":username,"password":password})
+  console.log(JSON.stringify(users,null,4))
+  return res.status(200).send("user "+username+" created")
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).send(JSON.stringify(books,null, 4))
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn=req.params.isbn
+  let book=books[isbn]
+  if(book){
+    return res.status(200).send(JSON.stringify(book,null,4))
+  }
+  return res.status(404).send("invalid parameter")
+
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author=req.params.author
+  let filteredBooks=Object.values(books).filter(book=>book.author==author)
+  if(filteredBooks.length>0){
+    return res.status(200).send(JSON.stringify(filteredBooks,null,4))
+  } 
+  return res.status(404).send("invalid parameter")
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let title=req.params.title
+    let filteredBooks=Object.values(books).filter(book=>book.title==title)
+    if(filteredBooks.length>0){
+      return res.status(200).send(JSON.stringify(filteredBooks,null,4))
+    } 
+    return res.status(404).send("invalid parameter")
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn=req.params.isbn
+  let book=books[isbn]
+  console.log(book)
+  if(book){
+    return res.status(200).send(JSON.stringify(book.reviews,null,4))
+  }
+  return res.status(404).send("invalid parameter")
 });
+
+
+const getBooks=async ()=>{
+let url="https://calaa-5000.theiadocker-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/"
+let req=await axios.get(url)
+let data=req.data
+console.log(data)
+}
+const getBookByIsbn=async ()=>{
+    let isbn="5"
+    let url="https://calaa-5000.theiadocker-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/isbn/"+isbn
+    let req=await axios.get(url)
+    let data=req.data
+    console.log(data)
+}
+const getBookByAuthor=async ()=>{
+    let author="Unknown"
+    let url="https://calaa-5000.theiadocker-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/author/"+author
+    let req=await axios.get(url)
+    let data=req.data
+    console.log(data)
+}
+const getBookByTitle=async ()=>{
+    let title="Fairy tales"
+    let url="https://calaa-5000.theiadocker-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/title/"+title
+    let req=await axios.get(url)
+    let data=req.data
+    console.log(data)
+}
+
 
 module.exports.general = public_users;
